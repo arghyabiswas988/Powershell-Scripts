2d5d0b112b4fbb2d82cf219f85fb3fd5fa705e9b
Arghya Biswas <3684168+arghyabiswas988@users.noreply.github.com>
2023-09-26 17:40:55 +0100

    Add files via upload

---
+++ Create-SymbolicLink.ps1
@@ -0,0 +1,26 @@
+﻿<#
+$Target = "\\DESKTOP-MSI\Storage1_4TB\Dispute.jpg"
+$Link = "C:\Temp\Dispute.jpg"
+New-Item -ItemType SymbolicLink -Path "$Link" -Target "$Target"
+
+#New-Item -Path "C:\Temp" -ItemType File -Name "test.mp4" -Force
+#>
+$Folder = "C:\Temp\Downloads\IDM\Video"
+
+$DesktopIni = @"
+[.ShellClassInfo]
+IconResource=C:\WINDOWS\System32\SHELL32.dll,115
+"@
+
+$TargetDirectory = $Folder
+#Test-Path (Join-Path "$TargetDirectory" "desktop.ini")
+
+#Remove-Item -Path "$($TargetDirectory)\desktop.ini" -Force
+#Create/Add content to the desktop.ini file
+Add-Content "$($TargetDirectory)\desktop.ini" -Value $DesktopIni -Force
+
+#Set the attributes for $DesktopIni
+(Get-Item "$($TargetDirectory)\desktop.ini" -Force).Attributes = 'Hidden, System, Archive'
+
+#Finally, set the folder's attributes
+(Get-Item $TargetDirectory -Force).Attributes = 'ReadOnly, Directory'
