fdc86cdfad0ffab34225dc123d365065b14e44a7
Arghya Biswas <3684168+arghyabiswas988@users.noreply.github.com>
2024-03-01 16:13:48 +0000

    Update getAppGRSHash.ps1

--- getAppGRSHash.ps1
+++ getAppGRSHash.ps1
@@ -1,24 +1,28 @@
-#https://call4cloud.nl/2022/07/retry-lola-retry/
-$appId = "a1d259fe-685d-4f75-bd6b-2785f9bb3688"
+#Source https://gist.github.com/ztrhgf
+function _getAppGRSHash {
+    param (
+        [Parameter(Mandatory = $true)]
+        [string] $appId
+    )
 
-$intuneLogList = Get-ChildItem -Path "$env:ProgramData\Microsoft\IntuneManagementExtension\Logs" -Filter "IntuneManagementExtension*.log" -File | sort LastWriteTime -Descending | select -ExpandProperty FullName
+    $intuneLogList = Get-ChildItem -Path "$env:ProgramData\Microsoft\IntuneManagementExtension\Logs" -Filter "IntuneManagementExtension*.log" -File | sort LastWriteTime -Descending | select -ExpandProperty FullName
 
-if (!$intuneLogList) {
-  Write-Error "Unable to find any Intune log files. Redeploy will probably not work as expected."
-  return
-  }
-foreach ($intuneLog in $intuneLogList) {
-  $appMatch = Select-String -Path $intuneLog -Pattern "\[Win32App\]\[GRSManager\] App with id: $appId is not expired." -Context 0, 1
-  if ($appMatch) {
-  foreach ($match in $appMatch) {
-    $Hash = “”
-    $LineNumber = 0
-    $LineNumber = $match.LineNumber
-    $Hash = Get-Content $intuneLog | Select-Object -Skip $LineNumber -First 1
-    if ($hash) {
-      $hash = $hash.Replace('+','\+')
-      return $hash
-      }
+    if (!$intuneLogList) {
+        Write-Error "Unable to find any Intune log files. Redeploy will probably not work as expected."
+        return
     }
-  }
+
+    foreach ($intuneLog in $intuneLogList) {
+        $appMatch = Select-String -Path $intuneLog -Pattern "\[Win32App\] ExecManager: processing targeted app .+ id='$appId'" -Context 0, 2
+        if ($appMatch) {
+            foreach ($match in $appMatch) {
+                $hash = ([regex]"\d+:Hash = ([^]]+)\]").Matches($match).captures.groups[1].value
+                if ($hash) {
+                    return $hash
+                }
+            }
+        }
+    }
+
+    Write-Error "Unable to find App '$appId' GRS hash in any of the Intune log files. Redeploy will probably not work as expected"
 }
