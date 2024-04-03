36b0298b461f368314da857c69825b625a3f710d
Arghya Biswas <3684168+arghyabiswas988@users.noreply.github.com>
2024-04-04 00:55:30 +0100

    Add files via upload

---
+++ Get-ChromeExtension_Working.ps1
@@ -0,0 +1,15 @@
+﻿function Get-ChromeExtension {
+    param (
+        [string]$ComputerName = $env:COMPUTERNAME
+    )
+
+    Get-ChildItem "\\$ComputerName\c$\users\*\appdata\local\Google\Chrome\User Data\*\Extensions\*\*\manifest.json" -ErrorAction SilentlyContinue | % {
+        $path = $_.FullName
+        #Write-Output $path
+        $_.FullName -match 'users\\(.*?)\\appdata' | Out-Null
+        #Get-Content $_.FullName -Raw | ConvertFrom-Json | select @{n='ComputerName';e={$ComputerName}}, @{n='User';e={$Matches[1]}}, Name, Version, @{n='Path';e={$path}}
+        Get-Content $_.FullName -Raw | ConvertFrom-Json | select Name, Version
+    }
+}
+
+Get-ChromeExtension | ? name -notmatch '__msg_' | sort Name, {[version]$_.version}
