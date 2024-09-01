77160bd47277f6eb87735c0231dafcdd97b47825
Arghya Biswas <3684168+arghyabiswas988@users.noreply.github.com>
2024-09-01 15:06:07 +0100

    Add files via upload

---
+++ Find_and-delete-folder-pattern.ps1
@@ -0,0 +1,15 @@
+﻿$Source = "G:\Hot"
+$pattern1 = '540'
+#Set-Location $Source
+cls
+$FolderList = Get-ChildItem -Path "$Source" -Exclude "*.*" -Recurse | Where-Object Name -like "*540*"
+
+Foreach ($Folder in $FolderList){
+    $FolderName = $Folder.Name
+    $FolderPath = $Folder.FullName
+    #Write-Host "$FolderName"
+    Write-Host "$FolderPath"
+    #$RemoveFolder = Join-Path $Source $FolderName
+    #Write-Host $RemoveFolder
+    Remove-Item -Path "$FolderPath" -Recurse -Force
+    }
