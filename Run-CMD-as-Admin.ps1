cedbffc68fef0e1ba46107b52a525f1e617ff8a4
Arghya Biswas <3684168+arghyabiswas988@users.noreply.github.com>
2025-10-31 02:55:34 +0530

    Update Run-CMD-as-Admin.ps1

--- Run-CMD-as-Admin.ps1
+++ Run-CMD-as-Admin.ps1
@@ -1,9 +1,10 @@
 ﻿# Construct the credentials object
-$username = "punisher"
+$username = "<username>"
 
 $password = Read-Host -Prompt "Pleease enter password for $username" -AsSecureString #Get-Credential -UserName $username -Message "Please enter password for $username" #ConvertTo-SecureString "test" -AsPlainText -Force    
 $cred = New-Object PSCredential -Args $username, $password
 #$cred = Get-Credential -UserName "$username" -Message "Enter password for $username"
 
 Start-Process powershell.exe -Credential $cred -WorkingDirectory C:\ -WindowStyle Hidden `
+
  '-noprofile -command "Start-Process cmd.exe -Verb RunAs"'
