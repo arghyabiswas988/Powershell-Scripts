e3bc0e284bab84e1ca01e27dc3bf4a61d43531cf
Arghya Biswas <3684168+arghyabiswas988@users.noreply.github.com>
2025-10-31 02:54:41 +0530

    Add files via upload

---
+++ Run-CMD-as-Admin.ps1
@@ -0,0 +1,9 @@
+﻿# Construct the credentials object
+$username = "punisher"
+
+$password = Read-Host -Prompt "Pleease enter password for $username" -AsSecureString #Get-Credential -UserName $username -Message "Please enter password for $username" #ConvertTo-SecureString "test" -AsPlainText -Force    
+$cred = New-Object PSCredential -Args $username, $password
+#$cred = Get-Credential -UserName "$username" -Message "Enter password for $username"
+
+Start-Process powershell.exe -Credential $cred -WorkingDirectory C:\ -WindowStyle Hidden `
+ '-noprofile -command "Start-Process cmd.exe -Verb RunAs"'
