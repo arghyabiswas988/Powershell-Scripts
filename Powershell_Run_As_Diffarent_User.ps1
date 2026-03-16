5c44dc93b476a21acac4ebe29df33634cb3e07b3
Arghya Biswas <3684168+arghyabiswas988@users.noreply.github.com>
2026-03-17 03:16:02 +0530

    Add files via upload

---
+++ Powershell_Run_As_Diffarent_User.ps1
@@ -0,0 +1,17 @@
+﻿# Prefill username
+$username = "desktop-msi02\Punisher"
+
+# Prompt for credentials
+$cred = Get-Credential -UserName $username -Message "Enter password for $username"
+
+# Command to run inside the new session
+$command = "Start-Process powershell_ise.exe; Start-Process powershell.exe -Verb RunAs"
+
+# Encode command for safe execution
+$bytes = [System.Text.Encoding]::Unicode.GetBytes($command)
+$encoded = [Convert]::ToBase64String($bytes)
+
+# Start PowerShell as Desktop\User1 and run the commands
+Start-Process powershell.exe `
+    -Credential $cred `
+    -ArgumentList "-NoProfile -EncodedCommand $encoded"
---
+++ Powershell_Run_As_Diffarent_User_v2.0.ps1
@@ -0,0 +1,22 @@
+﻿# Prefill username
+$username = "desktop-msi02\Punisher"
+
+# Prompt for credentials
+$cred = Get-Credential -UserName $username -Message "Enter password for $username"
+
+# Commands to run as the alternate user
+$command = @"
+\$ise = Start-Process powershell_ise.exe -PassThru
+Start-Sleep -Seconds 2
+Start-Process powershell.exe -Verb RunAs
+Stop-Process -Id \$ise.Id
+"@
+
+# Encode command
+$bytes = [System.Text.Encoding]::Unicode.GetBytes($command)
+$encoded = [Convert]::ToBase64String($bytes)
+
+# Run under the alternate credentials
+Start-Process powershell.exe `
+    -Credential $cred `
+    -ArgumentList "-NoProfile -EncodedCommand $encoded"
